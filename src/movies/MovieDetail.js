636b642d332c17efd3d07696972092c3cb3acb59
gwarz <gmwarzecha@tutanota.com>
2021-06-14 17:34:27 -0700

    set up the MovieDetail component to return data via the movie API and display it on the detail page

--- src/movies/MovieDetail.js
+++ src/movies/MovieDetail.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
+const BASE_URL = "https://api.themoviedb.org/3/movie/";
+const API_KEY = "?api_key=e140197f87c91e5e1952501c088ee92c";
 export function MovieDetail() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+
+  const getMovie = async () => {
+    try {
+      const res = await fetch(BASE_URL + id + API_KEY);
+      const newMovie = await res.json();
+      setMovie(newMovie);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, [id]);
+
+  if (!movie.title) return null
+
   return (
     <div>
-      <h1>Movie Detail!</h1>
+      <h1>{movie.title}</h1>
+      <p>{movie.overview}</p>
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
